b93557bfebc8bba813e207df9556ad663da2d5c2
Mehr Anand <89043467+mesanand@users.noreply.github.com>
2025-07-21 10:46:37 -0400

    Added Documentation

--- AutomatedPayroll/Viewer/gui.py
+++ AutomatedPayroll/Viewer/gui.py
@@ -1,41 +1,21 @@
 # Imports
-
 from nicegui import ui
 from datetime import datetime, timedelta
 
-
-        
-class ToggleButton(ui.button):
-
-        def __init__(self, *args, **kwargs) -> None:
-            super().__init__(*args, **kwargs)
-            self._state = False
-            self.on('click', self.toggle)
-
-        def toggle(self) -> None:
-            """Toggle the button state."""
-            self._state = not self._state
-            self.update()
-
-        def update(self) -> None:
-            self.props(f'color={"green" if self._state else "red"}')
-            super().update()
 """
-Toggle Anomalies
+Initalizes and centers the Brewster EMS banner to the center of the banner/header at height 100
 """
-
-
-
 with ui.header().classes('justify-center').props('height-hint=100') as header:
     with ui.tabs() as tabs:
-       
         brewster=ui.image('BrewsterTwo.png').classes('w-64')
 
 """
-Brewster png banner
+Dynamic method created to find out the past 4 weeks (month) of pay periods
+Initalized the date, created value for Saturday, and the saturday before
+Created structure for entire week, and iterated through dates in MonthDayYear format
+Returned these days as a list
 """
-
-def pay_periods_3months():
+def pay_periods_month():
     #Using datetime find today, find where saturday is, and 
     today = datetime.today()
     #List Saturday at 5
@@ -43,23 +23,37 @@ def pay_periods_3months():
     last_saturday = today - timedelta(days=days_since_saturday)
     # Make a list of dates
     pay_periods = []
-    for i in range(12):
+    for i in range(4):
         end_date = last_saturday - timedelta(weeks=i)
         start_date = end_date - timedelta(days=6)
         pay_period = f'{start_date.strftime("%m/%d/%y")} - {end_date.strftime("%m/%d/%y")}'
         pay_periods.append(pay_period)
+        # Return the list of dates
     return pay_periods
 
 
-pay_periods = pay_periods_3months()
+"""
+Created pay_periods to group the pay period dropdown lsit and as well the ability to download the data as a CSV file
+"""
+pay_periods = pay_periods_month()
 with ui.button_group():
     with ui.row().classes('w-full justify-between items-center'):
         with ui.dropdown_button('Pay Periods', auto_close=True):
             for period in pay_periods:
                 ui.item(period)
         ui.button('Download CSV', on_click=lambda: print('Connect backend CSV here'))
-        ToggleButton('Anomalies')
 
+
+""""
+Created Multiple examples of dummy Data
+
+This includes name, pay_period, date, reason, and level of severity indicated by a flag
+
+allowed sorting for each column, by either alphabetically or numerically through the user interface in the GUI
+
+Severity goes in levels of: 
+🟡(considered) -> 🟠(slight possibility) -> 🔴 (high probability)
+"""
 ui.table(
     columns=[
         {'name': 'name', 'label': 'Name', 'field': 'name', 'required': True, 'sortable': True},
@@ -81,21 +75,18 @@ ui.table(
             'pay_period': '00287',
             'date': '07/20/25',
             'reason': 'Overpaid (not IFT)',
-            'flag': '🟡'
+            'flag': '🟠'
         },
         {
             'name': 'Mark Brewster',
             'pay_period': '12345',
             'date': '08/25/27',
-            'reason': 'No error occured',
-            'flag': '🟢'
+            'reason': '0 trips, more than 7 hours',
+            'flag': '🟡'
         }    
     ]
 )
 
-""" 
-Dummy Data
-"""
 
 
 # Launch in native app
